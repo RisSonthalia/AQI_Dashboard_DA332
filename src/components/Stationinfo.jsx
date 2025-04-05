cecfae2219911a45d8ba371cd615dfb9a3c1275f
RisSonthalia <iitianrishab88@gmail.com>
2025-04-05 10:55:30 +0530

    added live aqi inidcator in stationinfo

--- src/components/Stationinfo.jsx
+++ src/components/Stationinfo.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import LiveAQIIndicator from './LiveAQIIndicator';
 
 const StationInfo = ({ selectedStation }) => {
   return (
+    
     <div className="station-info">
+      {/* Live AQI indicator */}
+      <LiveAQIIndicator />
       <h2>{selectedStation.name}</h2>
       <p>
         {selectedStation.city || 'Unknown'}, {selectedStation.state || 'Unknown'},{' '}
